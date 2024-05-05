85f02d352caf5b151d5c90024289307207103adb
kpuyam <kelvin.puyam@tigeranalytics.com>
2024-05-05 13:36:12 +0530

    Update production

--- production/data_cleaning.py
+++ production/data_cleaning.py
@@ -3,8 +3,8 @@
 The processors in this step, apply the various cleaning steps identified
 during EDA to create the training datasets.
 """
+import yaml
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
 
 from ta_lib.core.api import (
@@ -15,152 +15,115 @@ from ta_lib.core.api import (
     string_cleaning
 )
 from scripts import binned_selling_price
+'''with open('production/conf/logging/production.yml', 'r') as stream:
+    config = yaml.load(stream, Loader=yaml.FullLoader)
 
+logging.config.dictConfig(config)'''
 
-@register_processor("data-cleaning", "product")
-def clean_product_table(context, params):
-    """Clean the ``PRODUCT`` data table.
-
-    The table contains information on the inventory being sold. This
-    includes information on inventory id, properties of the item and
-    so on.
+@register_processor("data-cleaning", "housing")
+def clean_house_table(context, params):
     """
+    Cleans the ``HOUSING`` data table.
 
-    input_dataset = "raw/product"
-    output_dataset = "cleaned/product"
+    Parameters:
+    - context: Context object containing information about the execution environment.
+    - params: Additional parameters or configuration settings for the cleaning process.
+
+    Returns:
+    - housing_df_clean: The cleaned housing dataset.
+
+    Description:
+    This function cleans the housing data table by performing the following steps:
+    1. Loads the input dataset from the path specified by "raw/housing".
+    2. Identifies string columns in the dataset, excluding specific numerical columns.
+    3. Converts specific numerical columns to integer type.
+    4. Cleans string columns using a custom cleaning function called string_cleaning.
+    5. Saves the cleaned dataset to the path specified by "cleaned/housing".
+    6. Returns the cleaned housing dataset.
+
+    """
+    input_dataset = "raw/housing"
+    output_dataset = "cleaned/housing"
 
     # load dataset
-    product_df = load_dataset(context, input_dataset)
+    housing_df = load_dataset(context, input_dataset)
+    str_cols = list(
+        set(housing_df.select_dtypes('object').columns.to_list())
+        - set([
+            'longitude',
+            'latitude',
+            'housing_median_age',
+            'total_rooms',
+            'total_bedrooms',
+            'population',
+            'households',
+            'median_income',
+            'median_house_value'
+        ])
+    )
+    housing_df_clean = (
+        housing_df
+        # set dtypes
+        .change_type([
+            'housing_median_age',
+            'total_rooms',
+            'population',
+            'households',
+            'median_house_value'
+        ], np.int64)
 
-    product_df_clean = (
-        product_df
-        # set dtypes : nothing to do here
-        .passthrough()
-        .transform_columns(
-            product_df.columns.to_list(), string_cleaning, elementwise=False
-        )
-        .replace({"": np.NaN})
-        # drop unnecessary cols : nothing to do here
-        .coalesce(["color", "Ext_Color"], "color", delete_columns=True)
-        # drop unnecessary cols : nothing to do here
-        .coalesce(["MemorySize", "Ext_memorySize"], "memory_size", delete_columns=True)
-        # ensure that the key column does not have duplicate records
-        .remove_duplicate_rows(col_names=["SKU"], keep_first=True)
-        # clean column names (comment out this line while cleaning data above)
-        .clean_names(case_type="snake")
+        .transform_columns(str_cols, string_cleaning, elementwise=False)
     )
 
     # save the dataset
-    save_dataset(context, product_df_clean, output_dataset)
-
-    return product_df_clean
-
-
-@register_processor("data-cleaning", "orders")
-def clean_order_table(context, params):
-    """Clean the ``ORDER`` data table.
-
-    The table containts the sales data and has information on the invoice,
-    the item purchased, the price etc.
-    """
-
-    input_dataset = "raw/orders"
-    output_dataset = "cleaned/orders"
-
-    # load dataset
-    orders_df = load_dataset(context, input_dataset)
-
-
-    # list of columns that we want string cleaning op to be performed on.
-    str_cols = list(
-        set(orders_df.select_dtypes("object").columns.to_list())
-        - set(["Quantity", "InvoiceNo", "Orderno", "LedgerDate"])
-    )
-    orders_df_clean = (
-        orders_df
-        # set dtypes
-        .change_type(["Quantity", "InvoiceNo", "Orderno"], np.int64)
-        # set dtypes
-        .to_datetime("LedgerDate", format="%d/%m/%Y")
-        # clean string columns (NOTE: only handling datetime columns)
-        .transform_columns(str_cols, string_cleaning, elementwise=False)
-        # clean column names
-        .clean_names(case_type="snake").rename_columns({"orderno": "order_no"})
-    )
-
-    # save dataset
-    save_dataset(context, orders_df_clean, output_dataset)
-    return orders_df_clean
-
-
-@register_processor("data-cleaning", "sales")
-def clean_sales_table(context, params):
-    """Clean the ``SALES`` data table.
-
-    The table is a summary table obtained by doing a ``inner`` join of the
-    ``PRODUCT`` and ``ORDERS`` tables.
-    """
-    input_product_ds = "cleaned/product"
-    input_orders_ds = "cleaned/orders"
-    output_dataset = "cleaned/sales"
-
-    # load datasets
-    product_df = load_dataset(context, input_product_ds)
-    orders_df = load_dataset(context, input_orders_ds)
-
-    sales_df_clean = orders_df.merge(product_df, how="inner", on="sku")
-
-    save_dataset(context, sales_df_clean, output_dataset)
-    return sales_df_clean
+    save_dataset(context, housing_df_clean, output_dataset)
+    return housing_df_clean
 
 
 @register_processor("data-cleaning", "train-test")
 def create_training_datasets(context, params):
-    """Split the ``SALES`` table into ``train`` and ``test`` datasets."""
+    """
+    Split the housing table into train and test datasets.
 
-    input_dataset = "cleaned/sales"
-    output_train_features = "train/sales/features"
-    output_train_target = "train/sales/target"
-    output_test_features = "test/sales/features"
-    output_test_target = "test/sales/target"
-    
+    Parameters:
+    - context: Context object containing information about the execution environment.
+    - params: Additional parameters or configuration settings for the cleaning process.
+
+    Return: None
+
+    Input dataset: "cleaned/housing"
+    Output train features: "train/housing/features"
+    Output train target: "train/housing/target"
+    Output test features: "test/housing/features"
+    Output test target: "test/housing/target"
+
+    Description:
+    - Loads the cleaned housing dataset from the specified input dataset path.
+    - Splits the data using StratifiedShuffleSplit into train and test sets.
+    - Save the features to the specified output path for train and test features.
+    - Save the target to the specified output path for train and test target.
+    """
+    input_dataset = "cleaned/housing"
+    output_train_features = "train/housing/features"
+    output_train_target = "train/housing/target"
+    output_test_features = "test/housing/features"
+    output_test_target = "test/housing/target"
+
     # load dataset
-    sales_df_processed = load_dataset(context, input_dataset)
-
-    # creating additional features that are not affected by train test split. These are features that are processed globally
-    # first time customer(02_data_processing.ipynb)################
-    cust_details = (
-        sales_df_processed.groupby(["customername"])
-        .agg({"ledger_date": "min"})
-        .reset_index()
-    )
-    cust_details.columns = ["customername", "ledger_date"]
-    cust_details["first_time_customer"] = 1
-    sales_df_processed = sales_df_processed.merge(
-        cust_details, on=["customername", "ledger_date"], how="left"
-    )
-    sales_df_processed["first_time_customer"].fillna(0, inplace=True)
-
-    # days since last purchas(02_data_processing.ipynb)###########
-    sales_df_processed.sort_values("ledger_date", inplace=True)
-    sales_df_processed["days_since_last_purchase"] = (
-        sales_df_processed.groupby("customername")["ledger_date"]
-        .diff()
-        .dt.days.fillna(0, downcast="infer")
-    )
+    housing_df_processed = load_dataset(context, input_dataset)
 
     # split the data
     splitter = StratifiedShuffleSplit(
         n_splits=1, test_size=params["test_size"], random_state=context.random_seed
     )
-    sales_df_train, sales_df_test = custom_train_test_split(
-        sales_df_processed, splitter, by=binned_selling_price
+    housing_df_train, housing_df_test = custom_train_test_split(
+        housing_df_processed, splitter, by=binned_selling_price
     )
 
     # split train dataset into features and target
     target_col = params["target"]
     train_X, train_y = (
-        sales_df_train
+        housing_df_train
         # split the dataset to train and test
         .get_features_targets(target_column_names=target_col)
     )
@@ -171,7 +134,7 @@ def create_training_datasets(context, params):
 
     # split test dataset into features and target
     test_X, test_y = (
-        sales_df_test
+        housing_df_test
         # split the dataset to train and test
         .get_features_targets(target_column_names=target_col)
     )
@@ -179,3 +142,5 @@ def create_training_datasets(context, params):
     # save the datasets
     save_dataset(context, test_X, output_test_features)
     save_dataset(context, test_y, output_test_target)
+    
+print("Completed cleaning and splitting housing dataset")
--- production/scripts.py
+++ production/scripts.py
@@ -1,7 +1,111 @@
 """Module for listing down additional custom functions required for production."""
 
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
 import pandas as pd
 
 def binned_selling_price(df):
     """Bin the selling price column using quantiles."""
-    return pd.qcut(df["unit_price"], q=10)
+    return pd.qcut(df["median_house_value"], q=10)
+
+
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+
+class FeatureExtraction(BaseEstimator, TransformerMixin):
+
+    def __init__(self):
+        self.n_features_ = None
+        pass
+
+    def fit(self, X, y=None):
+        """
+        Fit the transformer to the data.
+
+        This method is intended to learn any information from the input data
+        necessary for the transformation.
+
+        Parameters
+        ----------
+        - X : numpy.ndarray
+            The input data to fit the transformer.
+        - y: optional
+
+        Returns
+        -------
+        - self: Object
+            The fitted transformer instance.
+        """
+        self.n_features_ = X.shape[1]
+        return self  # nothing else to do
+
+    def transform(self, X):
+        """
+        Transform the data by adding derived features.
+
+        This method creates new features by deriving them from existing columns, like rooms per household,
+        population per household, and bedrooms per room. It appends these new derived features
+        to the input data.
+
+        Parameters
+        ----------
+        - X: numpy.ndarray
+            The input data to transform.
+
+        Returns
+        -------
+        - numpy.ndarray
+            The transformed data with the new derived features.
+        """
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = \
+            X[:, population_ix] / X[:, households_ix]
+        bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+        return np.c_[
+            X,
+            rooms_per_household,
+            population_per_household,
+            bedrooms_per_room
+        ]
+
+    def inverse_transform(self, X, full_pipeline):
+        """
+        Inverse transforms the data based on the given pipeline.
+
+        Used to reconstruct the original data from the transformed state.
+        It applies inverse transformations from the standard scaler and one-hot encoder
+        to recover the original numeric and categorical features.
+
+        Parameters
+        ----------
+        - X : pandas.DataFrame
+            The transformed data to be inverse-transformed.
+        - full_pipeline: Pipeline Object
+            The pipeline containing the transformations applied to the data.
+
+        Returns
+        --------
+        - reconstructed_data: pandas.DataFrame
+            A DataFrame with the inverse-transformed data, which should resemble the original data before transformation.
+        """
+        std_scaler = full_pipeline.named_transformers_["num"]\
+            .named_steps["std_scaler"]
+        inverse_num = std_scaler.inverse_transform(X.iloc[:, :-5])
+        original_data = inverse_num[:, :-3]
+
+        onehot_encoder = full_pipeline.named_transformers_["cat"]
+        inverse_categ = onehot_encoder.inverse_transform(X.iloc[:, -5:])
+
+        reconstructed_data = pd.DataFrame(
+            original_data,
+            columns=["longitude",
+                     "latitude",
+                     "housing_median_age",
+                     "total_rooms",
+                     "total_bedrooms",
+                     "population",
+                     "households",
+                     "median_income"]
+        )
+        reconstructed_data["ocean_proximity"] = inverse_categ.flatten()
+
+        return reconstructed_data
